8e57ff85df3717dc5cabcd402ed191359eb808c2
Calapzu <yamsoncalapzu@gmail.com>
2021-09-26 20:12:21 -0500

    Se refactoriza la clase TennisGame3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,36 +1,88 @@
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private int pointPlayer2;
+    private int pointPlayer1;
+
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        if (joint()) {
+            return playerPoints();
         } else {
-            if (p1 == p2)
+            if (isDeuce()) {
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            }
+            return advantageOrWin();
         }
     }
-    
+
+    public String advantageOrWin() {
+        String player = "";
+        if (operation()) {
+            player = "Advantage " + higherPointPlayer();
+        } else {
+            player = "Win for " + higherPointPlayer();
+        }
+        return player;
+    }
+
+    public boolean operation() {
+        return ((pointPlayer1 - pointPlayer2) * (pointPlayer1 - pointPlayer2) == 1);
+    }
+
+    public String higherPointPlayer() {
+        String player = "";
+        if (pointPlayer1 > pointPlayer2) {
+            player = player1Name;
+        } else {
+            player = player2Name;
+        }
+        return player;
+    }
+
+    public String playerPoints() {
+        String score = "";
+
+        score = arrayPoints(pointPlayer1);
+        return (pointPlayer1 == pointPlayer2) ? score + "-All" : score + "-" + arrayPoints(pointPlayer2);
+
+    }
+
+    public boolean joint(){
+        return (playerPointIsEqual() && differentSumOfSix());
+    }
+
+    private boolean playerPointIsEqual() {
+        return (pointPlayer1 < 4 && pointPlayer2 < 4);
+    }
+
+    private boolean differentSumOfSix() {
+        return (!(pointPlayer1 + pointPlayer2 == 6));
+    }
+
+    private String arrayPoints(int pointPlayer1) {
+        String[] point = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        return point[pointPlayer1];
+    }
+
+    private boolean isDeuce() {
+        return pointPlayer1 == pointPlayer2;
+    }
+
+
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        if (playerName.equals("player1") )
+            this.pointPlayer1 += 1;
         else
-            this.p2 += 1;
-        
+            this.pointPlayer2 += 1;
+
     }
 
 }
